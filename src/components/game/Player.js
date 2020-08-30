8fad2f8321523c1e3294b4b176d3f1471c2f3f04
Chris Wagner <c.wagner7443@gmail.com>
2020-08-30 13:00:27 -0500

    added legal move checking

--- src/components/game/Player.js
+++ src/components/game/Player.js
@@ -1,11 +1,18 @@
 
 const Player = () => {
 
-    const receiveCoordsToAttack = (coords, gameboard) => {
+    const _receiveCoordsToAttack = (coords, gameboard) => {
         gameboard.receiveAttack(coords)
     }
 
-    return { receiveCoordsToAttack }
+    const isLegalMove = (coords, gameboard) => {
+        if(gameboard.returnGameboard()[coords[0]][coords[1]] === null) {
+            _receiveCoordsToAttack(coords, gameboard)
+        }
+        else {return false}
+    }
+
+    return { isLegalMove }
 }
 
 
--- src/test/game/player.test.js
+++ src/test/game/player.test.js
@@ -5,16 +5,16 @@ test("testing player attacking coordinates for a HIT", () => {
     const gameboard = Gameboard();
     gameboard.createShip([[0,0],[0,1]])
     const player = NewPlayer();
-    player.receiveCoordsToAttack([0,0],gameboard)
+    player.isLegalMove([0,0],gameboard)
 
     expect(gameboard.returnGameboard()[0][0]).toBe("HIT");
 })
 
 test("testing player attacking coordinates for a MISS", () => {
     const gameboard = Gameboard();
-    gameboard.createShip([[0,0],[0,1]])
+    gameboard.createShip([[0,0],[0,1]]);
     const player = NewPlayer();
-    player.receiveCoordsToAttack([0,4],gameboard)
+    player.isLegalMove([0,4],gameboard);
 
     expect(gameboard.returnGameboard()[0][0]).toBe(null);
 })
@@ -28,9 +28,24 @@ test("testing player attacking AI coordinates for a HIT and AI attcking Player f
     gameboards[0].createShip([[0,0],[0,1]]); // player
     gameboards[1].createShip([[0,0],[0,1]]); // AI
     const player = NewPlayer();
-    player.receiveCoordsToAttack([0,0],gameboards[1]);
-    player.receiveCoordsToAttack([0,5],gameboards[0]);
+    player.isLegalMove([0,0],gameboards[1]);
+    player.isLegalMove([0,5],gameboards[0]);
 
     expect(gameboards[1].returnGameboard()[0][0]).toBe("HIT");
     expect(gameboards[0].returnGameboard()[0][0]).toBe(null);
+})
+
+test("testing AI player's legal move to not attack in the same spot twice", () => {
+    const gameboards = [];
+    const gameboardP = Gameboard(); // player
+    const gameboardAI = Gameboard(); // AI
+    gameboards.push(gameboardP);
+    gameboards.push(gameboardAI);
+    gameboards[0].createShip([[0,0],[0,1]]); // player
+    gameboards[1].createShip([[0,0],[0,1]]); // AI
+    const playerAI = NewPlayer();
+    playerAI.isLegalMove([0,5],gameboards[0]);
+    
+    expect(playerAI.isLegalMove([0,5],gameboards[0])).toBeFalsy();
+    
 })
